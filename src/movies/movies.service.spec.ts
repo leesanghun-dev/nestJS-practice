b916648c46408c1b879e1c4e0ad55ef63837c9b6
LeeSangHun <tkdgns25300@naver.com>
2022-10-27 13:20:08 +0900

    feat: test getAll and getOne

---
+++ src/movies/movies.service.spec.ts
@@ -0,0 +1,53 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { MoviesService } from './movies.service';
+
+describe('MoviesService', () => {
+  let service: MoviesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MoviesService],
+    }).compile();
+
+    service = module.get<MoviesService>(MoviesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should be 4", () => {
+    expect(2 + 2).toEqual(4);
+  })
+
+  describe("getAll", () => {
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    })
+  })
+
+  describe("getOne", () => {
+    it("should return an movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ['test'],
+        year: 2000,
+      })
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    })
+
+    it("should throw 404 error", () => {
+      try {
+        service.getOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual("Movie with ID 999 not found.")
+      }
+    })
+  })
+
+});
